843128adeef5b393752b516b42aaa419cffa6320
Indika NK <indikank95@gmail.com>
2021-10-02 13:09:29 +0530

    Upload Config initial commit

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
+const fileUpload = require('express-fileupload')
 
 
 const {OAuth2Client} = require('google-auth-library');
 const CLIENT_ID = "138152370779-t2bqjq5llckbqfogf07dv3rd06c5chkq.apps.googleusercontent.com"
 const client = new OAuth2Client(CLIENT_ID);
-
+const { google } = require("googleapis");
+const path = require('path');
 
 
 const { Readable } = require('stream');
@@ -15,6 +17,11 @@ const { Readable } = require('stream');
 
 
 
+const CLIENT_ID_GOOGLE_DRIVE = '784148226618-qg33g8spinhgbcg79h2t2bb5db497gpn.apps.googleusercontent.com';
+const CLIENT_SECRET = 'DCzH9TjC97tlVjOi0QqJtykP';
+const REDIRECT_URL = 'https://developers.google.com/oauthplayground/';
+
+const REFRESH_TOKEN = '1//04slIllIF68i8CgYIARAAGAQSNwF-L9IrFPmFYE4LG-U13HQ3u9Dx0Aykvrq5ya17AJz7DV67FTHfQJjLAw6-T-y69Okv90Ad7EE';
 
 
 const PORT = process.env.PORT || 5000;
@@ -24,7 +31,9 @@ const PORT = process.env.PORT || 5000;
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(
+    fileUpload()
+)
 
 
  
@@ -79,8 +88,32 @@ function checkAuthentication(req, res, next){
       })
 
 }
+app.post('/single', async (req, res, next) => {
+    try {
+        const file = req.files.mFile
+        //console.log(file)
+        const fileName = new Date().getTime().toString() + path.extname(file.name)
+
+        if (file.truncated) {
+            throw new Error('File size is too big...')
+        }
+
+        uploadFile(file.data,fileName);
+        res.redirect('/success')
+
+
+    } catch (error) {
+        console.log(error)
+        res.send('Error uploading file')
+
+
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
 })
+
+
 
